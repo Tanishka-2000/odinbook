5cbecd38d17688341e3a10cfd7def63de96c42d8
Shashank Talwar <shashanktalwar81@gmail.com>
2023-01-19 20:40:11 +0530

    add dropdown on settings

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,13 +6,33 @@ export default function Navbar(){
     <nav>
 
       <div className='first'>
-      {/* shown in desktop view */}
+      {/* desktop view */}
         <li className='logo desktop-view'><a href='#'><span className='symbol'>B</span></a></li>
         <li className='search desktop-view'><span className='material-symbols-outlined search-icon'>search</span><input type='text' placeholder='Search odinbook' /></li>
-      {/* shown in mobile view */}
+      {/* mobile view */}
         <li className='logo-name mobile-view'><a href='#'><span className='name'>Odinbook</span></a></li>
         <li title='profile' className='mobile-view'><a href='#'><img className='account-img' src={userImage}/></a></li>
-        <li title='settings' className='mobile-view'><a href='#'><span className='material-symbols-outlined round-icon'>apps</span></a></li>
+        <li title='settings' className='mobile-view setting '>
+          <span className='material-symbols-outlined round-icon'>apps</span>
+          <div className='dropdown'>
+          <div>
+              <span className="material-symbols-outlined round-icon">bookmark_added</span>
+              saved posts
+              </div>
+            <div>
+              <span className="material-symbols-outlined round-icon">key</span>
+              change passwords
+            </div>
+            <div>
+              <span className="material-symbols-outlined round-icon">dark_mode</span>
+              dark mode
+            </div>
+            <div>
+              <span className="material-symbols-outlined round-icon">logout</span>
+              log out
+              </div>
+          </div>
+        </li>
       </div>
 
 
@@ -20,7 +40,7 @@ export default function Navbar(){
         <li title='home' className='active'><a href='#'><span className='material-symbols-outlined icon'>home</span></a></li>
         <li title='friends'><a href='#'><span className='material-symbols-outlined icon'>group</span></a></li>
         <li title='write post'><a href='#'><span className='material-symbols-outlined icon'>edit_square</span></a></li>
-        {/* add link on mobile view */}
+        {/* add link in mobile view */}
         <li title='notifications' className='mobile-view'><a><span className='material-symbols-outlined round-icon'>notifications</span></a></li>
       </div>
 
@@ -30,8 +50,28 @@ export default function Navbar(){
         <li className='find-friends' title='search friends'><a href='#'>Find Friends</a></li>
       {/* desktop view  */}
         {/* <li title='profile' className='desktop-view'><a href='#'><span className='material-symbols-outlined round-icon'>person</span></a></li> */}
+        <li title='settings' className='desktop-view setting'>
+          <span className='material-symbols-outlined round-icon'>apps</span>
+          <div className='dropdown'>
+            <div>
+              <span className="material-symbols-outlined round-icon">bookmark_added</span>
+              saved posts
+              </div>
+            <div>
+              <span className="material-symbols-outlined round-icon">key</span>
+              change passwords
+            </div>
+            <div>
+              <span className="material-symbols-outlined round-icon">dark_mode</span>
+              dark mode
+            </div>
+            <div>
+              <span className="material-symbols-outlined round-icon">logout</span>
+              log out
+              </div>
+          </div>
+        </li>
         <li title='profile' className='desktop-view'><a href='#'><img className='account-img' src={userImage}/></a></li>
-        <li title='settings' className='desktop-view'><a href='#'><span className='material-symbols-outlined round-icon'>apps</span></a></li>
         <li title='notifications' className='desktop-view'><a><span className='material-symbols-outlined round-icon'>notifications</span></a></li>
       </div>
 
