6fe6f82179400ca2e6330b8f7d3c153778f8dc2b
Shashank Talwar <shashanktalwar81@gmail.com>
2023-01-21 17:04:51 +0530

    add profile page for user

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,15 +1,16 @@
-import image1 from '../../images/image1.jpg';
-import image2 from '../../images/image2.jpg';
-import image3 from '../../images/image3.jpg';
-import Post from '../post/post.jsx';
+// import image1 from '../../images/image1.jpg';
+// import image2 from '../../images/image2.jpg';
+// import image3 from '../../images/image3.jpg';
+// import Post from '../post/post.jsx';
 import './styles.css';
-import Users from '../users/users';
+import Profile from '../profile/profie';
+// import Users from '../users/users';
 
 export default function Home(){
   return(
     <div className='home'>
     {/* on cliking add button should display new post form*/}
-      <div className='write'>
+      {/* <div className='write'>
         <span className="material-symbols-outlined add-btn">add</span>
         <div>
           <p className='bold'>Create Story</p>
@@ -18,8 +19,8 @@ export default function Home(){
       </div>
       <Post img={image1}/>
       <Post img={image2}/>
-      <Post img={image3}/>    
-      
+      <Post img={image3}/>     */}
+      <Profile />
       {/* <Users /> */}
     </div>
   )
---
+++ src/components/profile/profie.jsx
@@ -0,0 +1,62 @@
+import './styles.css';
+import userImage from '../../images/user.jpg';
+import cat2 from '../../images/cat2.jpg';
+import Post from '../post/post';
+
+export default function Profile(){
+  return(
+    <div className='profile'>
+
+      <div className='profile-image'>
+        <img src={userImage} />
+      </div>
+
+      <div className='name'>
+        <h1>Snow White</h1>
+        <p>In a cat's eye all things belong to cats.</p>
+      </div>
+
+      <button className='edit-btn'>
+        <span className='material-symbols-outlined'>edit</span>
+        Edit Profile
+      </button>
+
+      <div className='bio'>
+        <div>
+          <span className='material-symbols-outlined'>home_work</span>
+          <p>Current Town or City</p>
+        </div>
+
+        <div>
+          <span className='material-symbols-outlined'>work</span>
+          <p>Workplace</p>
+        </div>
+
+        <div>
+          <span className='material-symbols-outlined'>school</span>
+          <p>School or University</p>
+        </div>
+
+        <div>
+          <span className='material-symbols-outlined'>location_on</span>
+          <p>Home Town</p>
+        </div>
+
+        <div>
+          <span className='material-symbols-outlined'>favorite</span>
+          <p>Relationship status</p>
+        </div>
+      </div>
+
+      <button className='see-friends-btn'>
+        <span className='material-symbols-outlined'>group</span>
+        See Friends
+      </button>
+      
+      <div className='posts'>
+        <Post img={cat2}/>
+        <Post img={cat2}/>
+      </div>
+    </div>
+  )
+}
