3d64e7279e4857cba603e17db4d85b4b2032eb73
Shashank Talwar <shashanktalwar81@gmail.com>
2023-01-29 23:31:08 +0530

    post new comments

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -1,29 +1,21 @@
-import { useEffect, useState } from 'react';
-import { redirect } from 'react-router-dom';
+import { useState } from 'react';
+import { Form, redirect } from 'react-router-dom';
 import './styles.css';
-// import userImage from '../../images/user.jpg';
-// import cat1 from '../../images/cat1.jpg';
-// import cat2 from '../../images/cat2.jpg';
-// import cat3 from '../../images/cat3.jpg';
-
 
 export default function Post({post}){
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
 
   const loadComments = async () => {
-    if(showComments) {
-      setShowComments(false);
-      return;
-    }
-    console.log('loading');
-    console.log(post._id);
+
+    // if(showComments) return setShowComments(false);
     const response = await fetch(`http://localhost:3000/protected/posts/${post._id}/comments`,{
       method: 'get',
       headers: {
         Authorization: `Bearer ${localStorage.getItem('token')}`
       }
     });
+
     if(response.status >= 400) return redirect('/login');
     const data = await response.json();
     setComments(data);
@@ -62,32 +54,59 @@ export default function Post({post}){
       </div>
 
       <div className='btn-group'>
-        <button onClick={likePost}><span className={`material-symbols-outlined ${post.isLiked ? 'liked' : ''}`}>thumb_up</span>like</button>
-        <button onClick={loadComments}>
+        <button
+          className={post.isLiked ? 'liked' : ''}
+          onClick={e => {e.target.classList.toggle('liked'); likePost()}}
+          >
+          <span className={'material-symbols-outlined'}>thumb_up</span>
+          like
+        </button>
+
+        <button onClick={() => showComments ? setShowComments(false) : loadComments()}>
           <span className='material-symbols-outlined'>comment</span>
           comment
         </button>
       </div>
 
       {/* comments should be visible on clicking comment button */}
-      {showComments ? <Comments comments={comments}/> : ''}
+      {showComments ? <Comments comments={comments} loadComments={loadComments} postId={post._id}/> : ''}
   </div>
   )
 }
 
-function Comments({comments}){
+function Comments({comments, loadComments, postId}){
 
+  const [message, setMessage] = useState('');
+
+  const shareComment = async (e) => {
+    e.preventDefault();
+    console.log(message);
+    const response = await fetch(`http://localhost:3000/protected/posts/${postId}/comments`,{
+      method: 'post',
+      headers: {
+        'Content-Type': 'Application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      },
+      body: JSON.stringify({message})
+    });
+    loadComments()
+  }
+  
   return(
     <div className='comments'>
+
       <div className='write-comment'>
         <div><img className='account-img' src={localStorage.getItem('avatar')}/></div>
-        <form>
-          <textarea aria-label=' write comment' placeholder='write a comment...' rows={3}></textarea>
-          <button aria-label='share comment'><span className='material-symbols-outlined'>send</span></button>
+        <form onSubmit={shareComment}>
+          <textarea name='message' onChange={e => setMessage(e.target.value)} aria-label=' write comment' placeholder='write a comment...' rows={3}></textarea>
+          <button aria-label='share comment' type='submit'><span className='material-symbols-outlined'>send</span></button>
         </form>
       </div>
 
-      {comments.map(comment => <Comment comment={comment} />)}
+      {comments.length > 0 ?
+      comments.map(comment => <Comment comment={comment} />)
+      :
+      'No comments on this post...'}
     </div>  
   )
 }
