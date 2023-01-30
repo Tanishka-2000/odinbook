fc327777f26bc121a1c206745fa71e72f34c0936
Shashank Talwar <shashanktalwar81@gmail.com>
2023-01-30 12:14:33 +0530

    show error in post form and on click on close btn navigate to previous link

--- src/components/postForm/postForm.jsx
+++ src/components/postForm/postForm.jsx
@@ -1,11 +1,12 @@
-import { Form, redirect } from 'react-router-dom';
+import { Form, redirect, useActionData, useNavigate } from 'react-router-dom';
 import './styles.css';
 
 
 export async function postFormAction({request}){
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  console.log(data);
+  // console.log(data);
+
   let response = await fetch('http://localhost:3000/protected/posts',{
     method: 'post',
     headers: {
@@ -14,27 +15,48 @@ export async function postFormAction({request}){
     },
     body: JSON.stringify(data)
   });
-  // somethings wrong
-  if(response.status >= 400) return null;
+
+  console.log(response);
+  if(response.status >= 400){
+    response = await response.json();
+    return response;
+  }
+
   return redirect('/');
 }
 
 export default function PostForm(){
+
+  const error = useActionData();
+  const navigate = useNavigate();
+
   return(
     <div className='new-post'>
       {/* <div className='container'> */}
-        <p className='heading'>Create Post</p>
-        <span className="material-symbols-outlined close-btn" aria-label='close button'>close</span>
+        <div className='header'>
+          <p className='heading'>Create Post</p>
+          <button
+            aria-label='close button'
+            className=' close-btn'
+            onClick={() => navigate(-1)}
+          >
+            <span className="material-symbols-outlined">close</span>
+          </button>
+        </div>
+
         <div className='user'>
           <img src={localStorage.getItem('avatar')} />
           <p className='bold'>{localStorage.getItem('name')} Snow White</p>
         </div>
+
         <Form method='post'>
           <textarea rows='4' name='message' aria-label='write your message here' placeholder={`What's on your mind, ${localStorage.getItem('name')} ?`}></textarea>
           <input type='text' name='imageUrl' aria-label='Image URL' placeholder='add image url'/>
           <input  type='text' name='tags' aria-label='tags' placeholder='add tags'/>
           <button>Post</button>
         </Form>
+
+        {error ? <p className='error'>{error.msg}</p> : ''}
       {/* </div> */}
     </div>  
   )
